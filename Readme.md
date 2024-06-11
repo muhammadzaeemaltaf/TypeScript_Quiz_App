440fc01a5488156098e13e16a2fed30a76e1b5e8
muhammadzaeemaltaf <165184980+muhammadzaeemaltaf@users.noreply.github.com>
2024-06-12 00:37:34 +0500

    Add files via upload

---
+++ Readme.md
@@ -0,0 +1,7 @@
+# CLI Quiz Application
+
+A command-line quiz application built with Node.js, TypeScript, Inquirer, and Chalk.
+
+## Description
+
+This CLI-based quiz application prompts users for their name and then runs a quiz consisting of multiple-choice questions. After the quiz, it displays the user's score and whether each answer was correct or incorrect, highlighting correct answers in green and incorrect answers in red.
---
+++ index.js
@@ -0,0 +1,101 @@
+#!/usr/bin/env node
+import chalk from "chalk";
+import inquirer from "inquirer";
+const quiz = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris"],
+    },
+    {
+        question: "Which planet is known as the Red Planet?",
+        options: ["Earth", "Mars", "Jupiter"],
+    },
+    {
+        question: "Who wrote 'To Kill a Mockingbird'?",
+        options: ["Harper Lee", "Mark Twain", "Ernest Hemingway"],
+    },
+    {
+        question: "What is the smallest prime number?",
+        options: ["1", "2", "3"],
+    },
+    {
+        question: "What is the chemical symbol for water?",
+        options: ["O2", "H2O", "CO2"],
+    },
+    {
+        question: "What is the formula of acceleration?",
+        options: ["(vf + vi) / Δt", "(vf - vi) / Δt", "(vi - vf) / t"],
+    },
+    {
+        question: "Who is the father of computer?",
+        options: ["Charles Babbage", "Albert Einstien", "Alexander Graham Bell"],
+    },
+    {
+        question: "Which one is the hottest planet in our solar system?",
+        options: ["Mars", "Mercury", "Venus"],
+    },
+    {
+        question: "What is value of planck's constant?",
+        options: ["6.67 x 10^-34 J/s", "6.626 x 10^-34 Js", "6.626 x 10^-34m/s"],
+    },
+    {
+        question: "Which is the most essential element in organic chemistry?",
+        options: ["Carbon", "Hydrogen", "Oxygen"],
+    },
+];
+const correctAnswers = [
+    "Paris",
+    "Mars",
+    "Harper Lee",
+    "2",
+    "H2O",
+    "(vf - vi) / Δt",
+    "Charles Babbage",
+    "Venus",
+    "6.626 x 10^-34 Js",
+    "Carbon",
+];
+let ask = await inquirer.prompt([
+    {
+        name: "name",
+        type: "input",
+        message: "Enter your Name: ",
+        validate: (input) => {
+            if (!input) {
+                return "Please enter your name.";
+            }
+            else if (typeof input !== "string") {
+                return "Please enter your name in string.";
+            }
+            else {
+                return true;
+            }
+        },
+    },
+]);
+(async () => {
+    const userName = ask.name;
+    console.log(chalk.bold.green(`\nWelcome, ${userName}! Let's start the quiz.\n`));
+    let result = [];
+    let score = 0;
+    for (let i = 0; i < quiz.length; i++) {
+        let questions = quiz[i];
+        let answer = await inquirer.prompt([
+            {
+                name: "userAnswer",
+                type: "list",
+                message: `Q${i + 1}: ${questions.question}`,
+                choices: questions.options,
+            },
+        ]);
+        let isCorrect = answer.userAnswer == correctAnswers[i];
+        if (isCorrect) {
+            score++;
+        }
+        result.push({ question: questions.question, option: isCorrect });
+    }
+    console.log(chalk.bold.green(`\nQuiz completed! ${chalk.bold.gray("Dear " + userName + "! Your score is")} ${chalk.green(score)}/${chalk.green(quiz.length)}.\n`));
+    result.map((value, index) => {
+        console.log(`Q${index + 1}: ${value.question} - ${value.option ? chalk.green("Correct") : chalk.red("Incorrect! " + `The correct answer is ${chalk.yellowBright(correctAnswers[index])}`)}`);
+    });
+})();
